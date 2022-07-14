d2730165f19ab0469f25524e791d6db8bfc9f8d7
grzegorzpikus <grzegorz.pikus@gmail.com>
2022-07-14 20:13:32 +0100

    hashing algorithm ahs been added.

--- Authentication.py
+++ Authentication.py
@@ -1,25 +1,27 @@
 import csv
-import Checking_functions as chf
+import Checking_functions as Ch_f
 import User_class as Uc
+
 
 def sign_up():
     """Function that creates a new account."""
 
     def user_name_provider():
         username = input('provide your username: ')
-        if chf.user_exists(username) is True:
+        if Ch_f.user_exists(username) is True:
             print('User name already exists. Provide a different one: ')
             user_name_provider()
-        if chf.char_num_user(username) is False:
+        if Ch_f.char_num_user(username) is False:
             print('The user name is too short. Provide a different one: ')
             user_name_provider()
         return username
 
     def password_provider():
         password = input('provide your password: ')
-        if chf.char_num_password(password) is False:
+        if Ch_f.char_num_password(password) is False:
             print('The password is too short. Provide a different one: ')
             password_provider()
+        password = Ch_f.hashing(password)
         return password
 
     name = input('provide your name: ')
@@ -39,8 +41,8 @@ def log_in():
     user_name = input('username: ')
     password = input('password: ')
 
-    user_name_verification = chf.user_exists(user_name)
-    password_verification = chf.password_correct(password)
+    user_name_verification = Ch_f.user_exists(user_name)
+    password_verification = Ch_f.password_correct(user_name, password)
 
     if user_name_verification is True and password_verification is True:
         print('You successfully logged in.')
@@ -55,10 +57,11 @@ def log_in():
     with open('users.csv') as file:
         csv_reader = csv.reader(file, delimiter=',')
         for line in csv_reader:
-            if line[1] == password:
+            if line[0] == user_name:
                 user_object = Uc.User(line[2], line[3], line[0], line[1])
                 return user_object
 
-# sign_up()
+
+sign_up()
 # x = log_in()
 # print(x)
--- Checking_functions.py
+++ Checking_functions.py
@@ -1,5 +1,9 @@
 import csv
+import hashlib as hl
 
+def hashing(password):
+    password_hash = hl.sha256(str.encode(password)).hexdigest()
+    return password_hash
 
 def user_exists(user_name):
     """This function checks if user name exists. It returns true if it does,
@@ -14,17 +18,18 @@ def user_exists(user_name):
                 return False
 
 
-def password_correct(password):
+def password_correct(user_name, password):
     """This function checks if user name exists. It returns true if it does,
     otherwise it return false."""
 
     with open('users.csv') as file:
         csv_reader = csv.reader(file, delimiter=',')
         for line in csv_reader:
-            if line[1] == password:
-                return True
-            else:
-                return False
+            if line[0] == user_name:
+                if line[1] == hashing(password):
+                    return True
+                else:
+                    return False
 
 
 def char_num_user(string):
