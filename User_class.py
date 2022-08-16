6f473ee7d5ccbc3ed86f7aaee33b8934151fb444
grzegorzpikus <grzegorz.pikus@gmail.com>
2022-08-16 20:12:28 +0100

    Administrator subclass has been added.

--- User_class.py
+++ User_class.py
@@ -4,11 +4,12 @@ class User:
     """This class stores users details. After log-in the information about
     an user are kept in the object of this class."""
 
-    def __init__(self, __name, __email, __user_name, __password):
+    def __init__(self, __name, __email, __user_name, __password, admin=False):
         self.__name = __name
         self.__email = __email
         self.__user_name = __user_name
         self.__password = __password
+        self.__admin = admin
 
     def __str__(self):
         """It represents a object as a string if the string is printed."""
@@ -17,3 +18,11 @@ class User:
     def __repr__(self):
         """This is a graphical representation of an object"""
         return f"{self.__user_name}"
+
+
+class Administrator(User):
+
+    def __init__(self, __name, __email, __user_name, __password, admin=True):
+        super().__init__(__name, __email, __user_name, __password, admin)
+
+    # getters and setters have to be added. And only admin can use them!
